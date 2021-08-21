494865e399b38a4eaba451dce8d7dd16721d4774
gaurav <gaurav.verma.cloud@gmail.com>
2021-08-22 00:24:46 +0530

    added details for : and blocks

--- code/python/python_basics/python_0001_basics.py
+++ code/python/python_basics/python_0001_basics.py
@@ -1,3 +1,6 @@
+#All python comments are in #
+
+#All python objects have a type
 x = [1,2,3]
 print(type(x))
 
@@ -6,3 +9,7 @@ print(type(y))
 
 z = 42
 print(type(z))
+
+#blocks of code start after a tab, the block defining if, for etc end in :
+for x in range(5):
+    print(x)
