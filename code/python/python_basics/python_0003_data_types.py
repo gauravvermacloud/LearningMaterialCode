e31afa7228b6c619f96c3e7c06ea4fdd651ae218
gaurav <gaurav.verma.cloud@gmail.com>
2021-08-23 00:29:05 +0530

    added file fordata types and code for int

---
+++ code/python/python_basics/python_0003_data_types.py
@@ -0,0 +1,24 @@
+intX = 77
+
+floatF = 77.0
+
+boolB = True
+
+print(intX)
+print(floatF)
+print(boolB)
+
+#int constructor
+intX = int("75")
+print(intX)
+print(type(intX))
+
+#int in binary
+intX = 0b11001
+print(intX)
+print(type(intX))
+
+#int from float
+intX = int(3.5)
+print(intX)
+print(type(intX))
