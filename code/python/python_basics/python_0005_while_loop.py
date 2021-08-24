a77be3a049f019b498eb1f78f7d626ea7ca43b30
gaurav <gaurav.verma.cloud@gmail.com>
2021-08-24 10:49:55 +0530

    while loop

---
+++ code/python/python_basics/python_0005_while_loop.py
@@ -0,0 +1,17 @@
+#basic while loop
+
+x =7
+while(x>5):
+    print(x)
+    x = x-1
+    
+#while loop with break
+
+print()
+x=10
+
+while(True):
+    print(x)
+    x = x-1
+    if(x<5):
+        break
