f5f9333454ea14e71bf4f7dd4c415503adc7c73f
gaurav <gaurav.verma.cloud@gmail.com>
2021-08-24 10:47:25 +0530

    added file for if, elif

---
+++ code/python/python_basics/python_0004_relational_operator_Conditional_Statement.py
@@ -0,0 +1,19 @@
+if(2 < 4):
+    print("2 < 4")
+    
+if(2>4):
+    print(" inside if- 2>4")
+else:
+    print("inside else - 2<4")
+    
+    
+x = 35
+
+if(x>50):
+    print("x>50")
+elif (x>40):
+    print("x between 40 and 50")
+elif (x>30):
+    print("x between 30 and 40")
+else:
+    print("x is less than 30")
