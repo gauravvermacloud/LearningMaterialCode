e4b0d2f3c5219ac8b57a3e33a08791762151c6d3
gaurav <gaurav.verma.cloud@gmail.com>
2021-08-26 00:33:05 +0530

    added code for stings

---
+++ code/python/python_basics/python_0006_strings.py
@@ -0,0 +1,34 @@
+x ="hello" "world"
+print(x)
+
+x = "hello ' world"
+print (x)
+
+multi_line_sting_1 = """
+My
+multi
+line 
+string
+"""
+
+print(multi_line_sting_1)
+
+multi_line_sting_2 = '''
+My
+multi
+line 
+string2
+'''
+
+print(multi_line_sting_2)
+
+multi_line_string_3 ="My \n multi line \n string3"
+print(multi_line_string_3)
+
+
+raw_string = r'this is a raw string \  is as it is \\ so is double \ and \n is not an escape sequnce'
+print(raw_string)
+
+
+s = "This is a string"
+print (s[2])
