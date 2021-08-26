314efe415a7ac7012d2daf5bbc464b3e85ff7c78
gaurav <gaurav.verma.cloud@gmail.com>
2021-08-27 00:05:15 +0530

    added code for list

---
+++ code/python/python_basics/python_0008_list.py
@@ -0,0 +1,20 @@
+list_l = [1,2,3,4]
+list_y = ['a','b','c',]
+list_z = list("hello world")
+list_k = [1,"2",1.5]
+print (list_l)
+print (list_y)
+print (list_z)
+print(list_k)
+
+print (list_l [2])
+
+list_l[2]=-99
+print (list_l [2])
+
+empty_list = []
+print(empty_list)
+empty_list.append(5)
+print(empty_list)
+empty_list.append(6)
+print(empty_list)
