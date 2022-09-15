0188296d4ff57ccad4a07d6abef2437040c89acc
gaurav <gaurav.verma.cloud@gmail.com>
2022-09-15 20:32:30 +0530

    updated by adding more files

--- code/flask/flask-docker/apis/account_api.py
+++ code/flask/flask-docker/apis/account_api.py
@@ -6,3 +6,8 @@ account_api = Blueprint('account_api', __name__)
 @account_api.route("/account")
 def account_list():
     return jsonify([1, 2, 3, 4])
+
+
+@account_api.route("/account/id")
+def account_by_id():
+    return jsonify({"id": 1})
