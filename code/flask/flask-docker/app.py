f896e9646c8fd13bba8e0f12098cc6ee9a9d6c04
gaurav <gaurav.verma.cloud@gmail.com>
2022-11-21 20:44:16 +0530

    added ssl support

--- code/flask/flask-docker/app.py
+++ code/flask/flask-docker/app.py
@@ -12,6 +12,9 @@ import Scheduler
 import time
 from flask_cors import CORS
 from my_logger import wrap
+from gevent import pywsgi
+from OpenSSL import SSL
+
 
 app = Flask(__name__)
 app.register_blueprint(account_api)
@@ -48,12 +51,37 @@ def handle_bad_request(e):
 
 
 if __name__ == "__main__":
-    if(config_reader.server == "Flask"):
+    if(config_reader.server == "Flask"):  
         app.run(
             debug=config_reader.debug,
             host=config_reader.host,
             port=config_reader.port
         )
-    else:
+    if(config_reader.server == "Waitress"):
         serve(app, host=config_reader.host,
-              port=config_reader.port)
+              port=config_reader.port
+              )
+    if(config_reader.server == "Gevent"):
+        #pip3 install gevent
+        #https://serverfault.com/questions/224122/what-is-crt-and-key-files-and-how-to-generate-them
+        #openssl genrsa 2048 > host.key
+        #chmod 400 host.key
+        #openssl req -new -x509 -nodes -sha256 -days 365 -key host.key -out host.cert
+        server = pywsgi.WSGIServer(('0.0.0.0', 5000), app, 
+                                   keyfile='host.key', 
+                                   certfile='host.cert')
+        server.serve_forever()
+    
+
+# To run with gunicorn ->>>>>  gunicorn --bind 0.0.0.0:5000 app:app
+# To run with mod_wsgi ->>>>>>>>> mod_wsgi-express start-server app.py --processes 4
+
+# https://flask.palletsprojects.com/en/2.2.x/deploying/mod_wsgi/
+# https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-gunicorn-and-nginx-on-ubuntu-18-04
+# https://www.theserverside.com/blog/Coffee-Talk-Java-News-Stories-and-Opinions/How-to-setup-Nginx-reverse-proxy-servers-by-example
+
+# nginx
+# https://docs.nginx.com/nginx/admin-guide/load-balancer/http-load-balancer/
+# http://nginx.org/en/docs/http/load_balancing.html
+
+# https://dev.to/thetrebelcc/how-to-run-a-flask-app-over-https-using-waitress-and-nginx-2020-235c
