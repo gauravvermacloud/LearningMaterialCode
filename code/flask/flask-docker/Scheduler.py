7ffa8f289b15a422885bccfc5024f1850c2e1652
gaurav <gaurav.verma.cloud@gmail.com>
2022-12-30 22:39:42 +0530

    Added code for new scheduler and docker file. Also added link for hosting in lambda

--- code/flask/flask-docker/Scheduler.py
+++ code/flask/flask-docker/Scheduler.py
@@ -8,10 +8,11 @@ from my_logger import wrap
 import config_reader
 
 # https://apscheduler.readthedocs.io/en/3.x/userguide.html
+# https://apscheduler.readthedocs.io/en/3.x/modules/triggers/cron.html
 scheduler = BackgroundScheduler()
 
 executors = {
-    'default': ThreadPoolExecutor(5)
+    'default': ThreadPoolExecutor(1)
 }
 job_defaults = {
     'coalesce': False,
@@ -26,12 +27,19 @@ scheduler = BackgroundScheduler(
 @wrap()
 def myfunc():
     time.sleep(4)
-    #We need to set the user header for the job
-    print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$>>>>>>>>>>>>>> Every 1 sec " + str(datetime.now())+str(threading.get_ident()))
+    # We need to set the user header for the job
+    print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$>>>>>>>>>>>>>> Every 1 sec " +
+          str(datetime.now())+str(threading.get_ident()))
 
 
 scheduler.add_job(myfunc, 'interval', seconds=1,
                   id='my_job_id', max_instances=1)
 
-if(config_reader.is_scheduler_enabled):
+
+# Run everyday at 5: 30 AM
+scheduler.add_job(myfunc, 'cron', day_of_week='mon-sun',
+                  hour=5, minute=30, end_date='9999-12-31')
+
+
+if (config_reader.is_scheduler_enabled):
     scheduler.start()
